44e11d85fe32ab9d5548a01c9d244187a8e6c4a8
maxkuzn <m.a.kuzn@ya.ru>
2018-06-04 16:32:07 +0300

    prep

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def main():
     print("\n\n")
     print("Loadding model.")
     sess = tf.Session(graph=m.graph)
-    m.load_model(sess, '/home/max/programming/squad/squad/data/model/saved')
+    m.load_model(sess, MODEL_PATH)
     print("Start testing", test[0][0].shape[0] // 32 + int(test[0][0].shape[0] != 32), "batches")
 
     f1 = m.evaluate(sess,
